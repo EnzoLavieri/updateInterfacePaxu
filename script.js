769752f6237a4b44a31733c3b082c2524695704d
Enzo D Andrey Lavieri Yarid <128073322+EnzoLavieri@users.noreply.github.com>
2024-03-27 12:04:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var backgrounds = [
+    "/imgs/Fundo1.jpg",
+    "/imgs/Fundo2.jpg",
+    "/imgs/Fundo3.jpg",
+  ];
+
+  var previousBackground = localStorage.getItem("background");
+  var randomIndex;
+
+  do {
+    randomIndex = Math.floor(Math.random() * backgrounds.length);
+  } while (backgrounds[randomIndex] === previousBackground);
+
+  var selectedBackground = backgrounds[randomIndex];
+  localStorage.setItem("background", selectedBackground);
+
+  document.querySelector(".imagem").style.backgroundImage =
+    "url('" + selectedBackground + "')";
+});
